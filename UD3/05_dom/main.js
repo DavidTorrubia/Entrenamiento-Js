bfa347097297316df2d3bcc5c649cd68055e6708
DavidTorrubia <a25585@svalero.com>
2022-10-24 17:48:51 +0200

    Hasta el ejercicio 6.2 unidad 3

---
+++ UD3/05_dom/main.js
@@ -0,0 +1,10 @@
+
+function listDiv(){
+    result = document.getElementsByTagName("div")
+    console.log(result)
+    result = document.getElementsByClassName("buttons")
+    console.log(result)
+    console.log(result[1])
+    console.log(document.getElementsByTagName("div"))
+}
+ listDiv()
--- UD3/06_modify_dom/exercise01/main.js
+++ UD3/06_modify_dom/exercise01/main.js
@@ -1,5 +1,9 @@
 window.onload = function(e) {
     console.log('documento cargado')
 
-    // document.getElementsByTagName('h1')[0].innerText = 'Changed from JS!!'
+    document.getElementsByTagName('h2')[0].innerText = 'Changed from JS!!'
+    document.querySelectorAll("article.first").forEach(item => item.style.color = "green");
+
+    document.querySelectorAll("p")[0].style.background = "black";
+    document.querySelectorAll(".buttons button").forEach(item => item.style.color = "blue");
 }
