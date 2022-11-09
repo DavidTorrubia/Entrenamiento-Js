9cf31516ffa1d3b6633022fc4b5b6df6e6aadd8d
DavidTorrubia <a25585@svalero.com>
2022-11-09 20:53:53 +0100

    Ejercicios hasta el UD-03 Ejercicio 13

--- UD3/09_ajax/main.js
+++ UD3/09_ajax/main.js
@@ -1,14 +1,20 @@
-window.onload = function() {
-    console.log('document loaded');
-
-    var xhttp = new XMLHttpRequest();   
-
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText)
-        }
-      };
-
+var xhttp = new XMLHttpRequest();
+window.onload = function () {
+  console.log("document loaded");
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
+      let employees = JSON.parse(this.responseText).data;
+      let tabla = document.getElementById("Tabla");
+      employees.forEach((item) => {
+        tabla.innerHTML += `<tr><td>${item.employee_name}</td><td>${item.employee_salary}</td><td>${item.employee_age}</td></tr>`;
+      });
+    }
+  };
+  xhttp.open("GET", "http://dummy.restapiexample.com/api/v1/employees", true);
+  xhttp.send();
+};
+function recharge() {
     xhttp.open("GET", "http://dummy.restapiexample.com/api/v1/employees", true);
-    xhttp.send();
 }
+
--- UD3/13_poo_rectangulo/rectangle.js
+++ UD3/13_poo_rectangulo/rectangle.js
@@ -1,9 +1,13 @@
 class Rectangle {
 
     // add x1, y1
-    constructor(x, y) {
+    constructor(x, y, width,height) {
         this.x = x;
         this.y = y;
+        this.topLeft = new point((x-(width/2)),(y+(height/2)))
+        this.bottomRight = new point((x+(width/2)),(y-(height/2)))
+        this.width = width;
+        this.height = height;
     }
 
     print() {
@@ -11,23 +15,37 @@ class Rectangle {
     }
 
     // area
+    GetArea(){
+        return this.width * this.height;
+    }
 
     // overlay
-    // tenemos que comprobar la siguiente situación
-    // .x----------x.....
-    // .|..........|.....
-    // .|......x---|----x
-    // .x------|---x....|
-    // ........x--------x
-    // Se superponen si alguna de sus coordenadas están dentro de los límites
-    // del otro rectángulo
+    isOverlay(rect){
+        if (this.topLeft.x == this.bottomRight.x || this.topLeft.y == this.bottomRight.y || rect.topLeft.x == rect.bottomRight.x || rect.topLeft.y == rect.bottomRight.y){
+            return false;
+        }
+        if (this.topLeft.x > rect.bottomRight.x || rect.topLeft.x > this.bottomRight.x) {
+            return false;
+        }
+
+        if (this.bottomRight.y > rect.topLeft.y || rect.bottomRight.y > this.topLeft.y) {
+            return false;
+        }
+        return true;
+    }
 }
 
-let r1 = new Rectangle(5,9);
-let r2 = new Rectangle(3,4);
+class point{
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+    }
+}
 
-r1.print()
-r2.print()
+let r1 = new Rectangle(1.3,2,1,1);
+let r2 = new Rectangle(1,1,1,1);
+
+console.log(r2.isOverlay(r1));
 
 // let area = r1.area();
 // console.log(`area ${area}`)
